156cc6eb1caabac2fbd6baea80c455df0ff4586a
Sazzad Hossen <thesazzadsh@gmail.com>
2021-05-21 21:36:27 +0600

    spring security demo api

---
+++ src/main/java/com/security/demo/config/ApplicationSecurityConfig.java
@@ -0,0 +1,53 @@
+package com.security.demo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/h2-console/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails john = User.builder()
+                .username("john")
+                .password(passwordEncoder.encode("1234"))
+                .roles("author").build();
+
+        return new InMemoryUserDetailsManager(
+                john
+        );
+    }
+}
---
+++ src/main/java/com/security/demo/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.security.demo.repository;
+
+import com.security.demo.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
