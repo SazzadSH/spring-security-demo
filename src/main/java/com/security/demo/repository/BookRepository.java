e30891c8d22578c4c7876337c152bae5e18a7b04
Sazzad Hossen <thesazzadsh@gmail.com>
2021-05-27 16:48:25 +0600

    SAML authentication using OKTA

--- src/main/java/com/security/demo/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.security.demo.repository;
-
-import com.security.demo.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
